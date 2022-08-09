9dc211dd824cd3c8228f77597bfeed6a8905ab19
gugray <gabor@berlin.de>
2022-08-09 22:38:06 +0200

    file system; download over https; update from downloaded file

--- code-board/src/main.cpp
+++ code-board/src/main.cpp
@@ -1,6 +1,18 @@
 #include <Arduino.h>
 #include <DOG7565R.h>
+#include <LittleFS.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
 #include "canvas.h"
+
+const char *fnFirmware = "firmware.bin";
+const char *wifiNetwork = "Cirrus";
+const char *wifiPassword = "brouhaha";
+const uint32_t wifiConnectTimeout = 10000;
+const char *fileUrl = "https://zydeo.net/firmware.bin";
+
+#define LED_PIN LED_BUILTIN
 
 #define DOG_CS_PIN D8
 #define DOG_A0_PIN D3
@@ -8,9 +20,14 @@
 
 DOG7565R dog;
 Canvas canvas;
+BearSSL::WiFiClientSecure secureBearClient;
+
+const size_t bufSize = 1024;
+char buf[bufSize];
 
 void flushCanvasToDisplay()
 {
+
   if (!canvas.dirty)
     return;
   canvas.dirty = false;
@@ -25,12 +42,110 @@ void flushCanvasToDisplay()
   }
 }
 
+size_t downloadFile(const char *url, const char *fn)
+{
+  if (LittleFS.exists(fn))
+    LittleFS.remove(fn);
+  File f = LittleFS.open(fn, "w");
+  if (!f)
+  {
+    Serial.printf("Failed to create file\n");
+    return 0;
+  }
+
+  HTTPClient http;
+  if (!http.begin(secureBearClient, url))
+  {
+    Serial.printf("http.begin() failed\n");
+    f.close();
+    return 0;
+  }
+
+  int16_t httpCode = http.GET();
+  Serial.printf("http.GET() returned: %d\n", httpCode);
+  if (httpCode != HTTP_CODE_OK && httpCode != HTTP_CODE_MOVED_PERMANENTLY)
+  {
+    f.close();
+    http.end();
+    return 0;
+  }
+
+  // Get length of document (is -1 when Server sends no Content-Length header)
+  int32_t len = http.getSize();
+  WiFiClient *stream = http.getStreamPtr();
+  // Read all data from server
+  while (http.connected() && (len > 0 || len == -1))
+  {
+    yield();
+    int32_t size = stream->available();
+    if (size <= 0)
+      continue;
+    if (size > (int32_t)bufSize)
+      size = bufSize;
+    size_t nRead = stream->readBytes(buf, size);
+    if (nRead == 0)
+      break;
+    if (len > 0)
+      len -= nRead;
+    f.write(buf, nRead);
+  }
+
+  size_t sz = f.size();
+  Serial.printf("Downloaded %d bytes\n", sz);
+
+  f.close();
+  http.end();
+  return sz;
+}
 
 void setup()
 {
-  pinMode(D1, OUTPUT);
-  digitalWrite(D1, HIGH);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
   delay(500);
+
+  Serial.begin(9600);
+  Serial.printf("Hello, world.\n");
+
+  LittleFS.begin();
+
+  WiFi.begin(wifiNetwork, wifiPassword);
+  Serial.printf("Connecting to WiFi...\n");
+  int8_t wifiStatus = WiFi.waitForConnectResult(wifiConnectTimeout);
+  if (wifiStatus == WL_CONNECTED)
+  {
+    Serial.printf("Connected to WiFi\n");
+    String ipStr = WiFi.localIP().toString();
+    Serial.printf("Local IP: %s\n", ipStr.c_str());
+    secureBearClient.setInsecure();
+    size_t updateSize = downloadFile(fileUrl, fnFirmware);
+    if (updateSize > 0)
+    {
+      Serial.printf("Updating from file\n");
+      File f = LittleFS.open(fnFirmware, "r");
+      Update.begin(updateSize);
+      Update.writeStream(f);
+      if (!Update.end())
+      {
+        Serial.printf("Error: %d\n", Update.getError());
+      }
+      else
+      {
+        Serial.printf("Update finished; restarting. See you on the other side!\n");
+        ESP.restart();
+      }
+    }
+  }
+  else
+    Serial.printf("Not connected to WiFi; status: %d\n", wifiStatus);
+
+  // File f = LittleFS.open("test.txt", "r");
+  // while (f.available())
+  // {
+  //   f.readBytesUntil('\n', buf, bufSize);
+  //   Serial.printf(buf);
+  // }
+  // f.close();
 
   // analogWriteRange(1023);
   // analogWriteFreq(880);
@@ -40,14 +155,14 @@ void setup()
   canvas.fwText(20, 7, "Hello world!");
   flushCanvasToDisplay();
 
-  digitalWrite(D1, LOW);
+  digitalWrite(LED_PIN, LOW);
   delay(500);
 }
 
 void loop()
 {
-  digitalWrite(D1, HIGH);
+  digitalWrite(LED_PIN, HIGH);
   delay(500);
-  digitalWrite(D1, LOW);
+  digitalWrite(LED_PIN, LOW);
   delay(500);
 }
